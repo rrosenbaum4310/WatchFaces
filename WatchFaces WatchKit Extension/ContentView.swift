c96d6633d28b2389510fc5b000169ce59cb52e2b
rrosenbaum <rrosenbaum4310@gmail.com>
2022-03-17 14:13:20 -0400

    Initial Commit

---
+++ WatchFaces WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchFaces WatchKit Extension
+//
+//  Created by Ryan Rosenbaum on 3/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchFaces WatchKit Extension/WatchFacesApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchFacesApp.swift
+//  WatchFaces WatchKit Extension
+//
+//  Created by Ryan Rosenbaum on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchFacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
